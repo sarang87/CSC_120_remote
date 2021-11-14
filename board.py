4cc88a76739bcb8ae7ac2d34a12f803bbf414302
Sarang <sarang87@vt.edu>
2021-11-14 17:36:11 -0500

    Board Printing functionality added

---
+++ board.py
@@ -0,0 +1,11 @@
+board = [["-" for i in range(3)] for j in range(3)]
+def print_board():
+    
+    print("Printing board")
+    for row in board:
+        print(row)
+
+ 
+
+if __name__ == "__main__":
+    print_board()
